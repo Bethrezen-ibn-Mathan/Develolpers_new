4ae76b7e96e156761b70f30316bd6828c055d7a5
Bethrezen-ibn-Mathan <42974842+Bethrezen-ibn-Mathan@users.noreply.github.com>
2018-09-15 17:45:51 +0300

    first commit

---
+++ GH_Start.bat
@@ -0,0 +1,12 @@
+@echo on
+echo "Автоматизация, - наше всё!"
+mkdir /e/GitHub/Develolpers_new
+cd /e/GitHub/Develolpers_new
+pause
+git init
+pause
+git add GH_Start.txt
+pause
+echo "git add ." > GH_Update.bat
+echo "git commit" > GH_Commit.bat
+pause
