66ef1431af3bf05a71a5575bf114268dfa56b178
Bethrezen-ibn-Mathan <42974842+Bethrezen-ibn-Mathan@users.noreply.github.com>
2018-09-15 21:00:06 +0300

    new commit

--- GH_Full.bat
+++
@@ -1,7 +0,0 @@
-mkdir /d/MyProject
-cd /d/MyProject
-git init
-echo "Read me, please! I have an important information for you!" > readme.txt
-git add readme.txt
-git commit -m "add readme file"
-
--- GH_Start.bat
+++ GH_Start.bat
@@ -1,5 +1,7 @@
-cd /e/GitHub/Develolpers_new
+mkdir /d/MyProject
+cd /d/MyProject
 git init
-git add GH_Start.txt
-echo "git add ." > GH_Update.bat
-git commit -m "first commit"
+echo "Read me, please! I have an important information for you!" > file.txt
+git add .
+git commit -m "add readme file"
+
